cb8086a78959fe08d1043596ae1f35d24ee9661a
Dinesh <dinesh.casul@gmail.com>
2023-08-20 00:36:42 +0530

    minor fixes

--- src/Components/ContactEmail.jsx
+++ src/Components/ContactEmail.jsx
@@ -47,48 +47,55 @@ export const ContactEmail = ({ setIsSuccess = () => {} }) => {
   return (
     <>
       <div className="sm:h-5/6 sm:w-full flex justify-center flex-col items-center shadow-brown ">
-        <div className="flex flex-col w-auto  sm:pl-[80px] items-start justify-start py-[12px]">
-
-          <h1 className="sm:text-[30px] mb-[30px] text-[40px] text-black font-bold">
+        <div className="flex flex-col w-auto  items-start justify-start py-[12px]">
+          <h1 className="sm:text-[100px] font-mainFamily mb-[30px] text-[40px] text-black font-bold">
             Get in touch!
           </h1>
         </div>
         <form className="w-[80%] flex flex-col" ref={form} onSubmit={sendEmail}>
           <div className="flex flex-col mb-3">
-            <label>Name</label>
+            {/* <label>Name</label> */}
             <input
-              className="bg-skin appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-white focus:border-brown"
+              className="bg-skin appearance-none rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-darkSkin focus:border-brown"
               type="text"
               name="from_name"
               value={formData.from_name}
               onChange={handleFormData}
+              placeholder="Enter Your Name"
+              autocomplete={"off"}
+              required
             />
           </div>
           <div className="flex flex-col mb-3">
-            <label>Email</label>
+            {/* <label>Email</label> */}
             <input
               type="email"
-              className="bg-skin appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-white focus:border-brown"
+              className="bg-skin appearance-none rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-darkSkin focus:border-brown"
               name="from_email"
               value={formData.from_email}
               onChange={handleFormData}
+              placeholder="Enter Your Email"
+              autocomplete={"off"}
+              required
             />
           </div>
           <div className="flex flex-col mb-3">
-            <label>Message</label>
+            {/* <label>Message</label> */}
             <textarea
               placeholder="Write your thoughts here..."
-              className="bg-skin appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-white focus:border-brown"
+              className="bg-skin appearance-none rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-darkSkin"
               rows="6"
               cols="80"
               name="message"
               value={formData.message}
               onChange={handleFormData}
+              autocomplete={"off"}
+              required
             />
           </div>
           <div className="flex items-center justify-center mt-[20px]">
             <button
-              className="p-[10px] w-[30%] sm:text-[18px] text-[14px] font-bold bg-skin border rounded hover:bg-white"
+              className="text-black p-[10px] w-[30%] sm:w-[10%] sm:text-[18px] text-[14px] font-normal bg-skin rounded hover:bg-darkSkin"
               type="submit"
               value="Send"
             >
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,12 @@ export default {
       black: "#000000",
       brown: "#703E32",
       skin: "#F0E8D5",
+      darkSkin:'#D8CDB6',
       lightbrown: "#997337",
     },
+    fontFamily: {
+      mainFamily : ['Karla', 'sans-serif']
+    }
   },
   plugins: [],
 };
