b16584345b1a936017688db164b7f86761cad5ec
Dinesh <dinesh.casul@gmail.com>
2023-08-20 12:39:06 +0530

    modified contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,12 +3,20 @@ import {ContactEmail} from './ContactEmail'
 const Contact = ({isSuccess, setIsSuccess = () => {}}) => {
   const windowSize = useWindowWidth();
 
-  const front_sm = "url(assets/grid-images.png)";
-  const front_lg = "url(assets/grid-images.png)";
+  const front_sm = "url(assets/contact-bg-sm.png)";
+  const front_lg = "url(assets/contact-bg.png)";
   return (
     <section id="#contact" className="w-screen h-screen">
+       <div
+          className=" h-screen w-full absolute z-[-2]"
+          style={{
+            backgroundSize: "cover",
+            backgroundImage: `${windowSize == "s" ? front_sm : front_lg}`,
+          }}
+        />
+
       <div
-        className={`flex absolute ${
+        className={`flex bg-image absolute ${
           windowSize == "s" ? "flex-col" : "flex-row"
         } h-screen w-full items-center justify-center z-1 overflow-hidden `}
       >
--- src/Components/ContactEmail.jsx
+++ src/Components/ContactEmail.jsx
@@ -48,20 +48,25 @@ export const ContactEmail = ({ isSuccess, setIsSuccess = () => {} }) => {
     <>
       <div className="sm:h-5/6 sm:w-full flex justify-center flex-col items-center shadow-brown ">
         <div className="flex flex-col w-auto  items-start justify-start py-[12px]">
-          <h1 className="sm:text-[100px] font-mainFamily mb-[30px] text-[40px] text-black font-bold">
+        
+          <h1 className="sm:text-[100px] font-mainFamily text-[40px] text-white font-bold">
             Get in touch!
           </h1>
+          <h1 className="sm:text-[20px] font-mainFamily mb-[20px] text-[10px] text-white font-bold">
+          Let's Transform Your Vision into Architectural Masterpieces Together!
+          </h1>
         </div>
+        <div className="sm:w-3/5 w-full flex items-center justify-center">
         <form className="w-[80%] flex flex-col" ref={form} onSubmit={sendEmail}>
           <div className="flex flex-col mb-3">
             {/* <label>Name</label> */}
             <input
-              className="bg-skin appearance-none rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-darkSkin focus:border-brown"
+              className="bg-black appearance-none rounded w-full py-2 px-4 text-skin leading-tight focus:outline-none focus:bg-brown focus:border-brown"
               type="text"
               name="from_name"
               value={formData.from_name}
               onChange={handleFormData}
-              placeholder="Enter Your Name"
+              placeholder="your name please.."
               autocomplete={"off"}
               required
             />
@@ -70,11 +75,11 @@ export const ContactEmail = ({ isSuccess, setIsSuccess = () => {} }) => {
             {/* <label>Email</label> */}
             <input
               type="email"
-              className="bg-skin appearance-none rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-darkSkin focus:border-brown"
+              className="bg-black appearance-none rounded w-full py-2 px-4 text-skin leading-tight focus:outline-none focus:bg-brown focus:border-brown"
               name="from_email"
               value={formData.from_email}
               onChange={handleFormData}
-              placeholder="Enter Your Email"
+              placeholder="your email please.."
               autocomplete={"off"}
               required
             />
@@ -82,8 +87,8 @@ export const ContactEmail = ({ isSuccess, setIsSuccess = () => {} }) => {
           <div className="flex flex-col mb-3">
             {/* <label>Message</label> */}
             <textarea
-              placeholder="Write your queries here..."
-              className="bg-skin appearance-none rounded w-full py-2 px-4 text-brown leading-tight focus:outline-none focus:bg-darkSkin"
+              placeholder="your queries here..."
+              className="bg-black appearance-none rounded w-full py-2 px-4 text-skin leading-tight focus:outline-none focus:bg-brown"
               rows="6"
               cols="80"
               name="message"
@@ -95,20 +100,22 @@ export const ContactEmail = ({ isSuccess, setIsSuccess = () => {} }) => {
           </div>
           <div className="flex items-center flex-col justify-center mt-[5px]">
             <button
-              className="text-black p-[4px] w-[30%] sm:w-[8%] sm:text-[18px] text-[14px] font-normal bg-skin rounded hover:bg-darkSkin"
+              className="text-white p-[4px] w-[30%] sm:w-[8%] sm:text-[18px] text-[14px] font-normal bg-black rounded hover:bg-brown"
               type="submit"
               value="Send"
             >
               Submit
             </button>
-            <span className="pl-[4px] mt-[6px] flex items-center` justify-center">
+            <span className="pl-[4px] text-white mt-[6px] flex items-center` justify-center">
             or reach us on 
             <a className="pointer hover:text-[green] font-bold ml-[6px]"  rel="noopener noreferrer" href={'https://api.whatsapp.com/send?phone=09897210076'} target={'_blank'}> Whatsapp 
              </a>
-             <span><img className="ml-[4px]" style={{width:'14px'}} src="assets\whatsapp.png"/></span>
+             <span className="flex items-center"><img className="ml-[4px]" style={{width:'14px'}} src="assets\whatsapp.png"/></span>
             </span>
           </div>
         </form>
+            </div>
+      
       </div>
     </>
   );
